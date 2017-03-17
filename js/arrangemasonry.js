3123eff263a0b9686c1a866513301db542d6c9e7
Natalia <natalia777mail@gmail.com>
2017-03-17 19:30:58 +0200

    Fixed masonry on first load

---
+++ js/arrangemasonry.js
@@ -0,0 +1,6 @@
+            $('.grid').masonry({
+                itemSelector: '.grid-item',
+                // use element for option
+                columnWidth: '.grid-sizer',
+                percentPosition: true            
+            });
